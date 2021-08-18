278a143a8815570e5657a4a16e869280a60d24c7
arturmarcosti <62904142+arturmarcosti@users.noreply.github.com>
2021-08-18 13:08:47 -0300

    Update dynamic

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "web"{
     
     tags = {
         Name = "PetClinic"
+        type = "master"
   }
 }
 
@@ -19,36 +20,23 @@ resource "aws_key_pair" "petclinic" {
 
 ## os Cidr_blocks estão para aceitar qualquer tipo de conexão apenas por se tratar de ambiente de homologação.
 resource "aws_security_group" "petclinic" {
-   ingress = {
-     from_port = 0
-     protocol = "-1"
-     self = true
-     to_port = 0
-   }
-}
-resource "aws_security_group" "petclinic" {
-    ingress = {
-     cidr_blocks = [ "0.0.0.0/0" ]
-     from_port = 22
-     protocol = "tcp"
-     to_port = 22
-   }
-}
+    
+    dynamic "ingress" {
+        for_each = var.default_ingress
+        content {
+            description = ingress.voluel["description"]
+            from_port = ingress.key
+            to_port = ingress.key
+            protocol = "tcp"
+            cidr_blocks = ingress.value["cidr_blocks"]
+        }
+    }
 
-resource "aws_security_group" "petclinic" {
-   ingress = {
-     cidr_blocks = [ "0.0.0.0/0" ]
-     from_port = 8080
-     protocol = "tcp"
-     to_port = 8080
-   }
-}
-
-resource "aws_security_group" "petclinic" {
-   egregress = {
+    egress = {
      cidr_blocks = [ "0.0.0.0/0" ]
      from_port = 0
      protocol = "-1"
      to_port = 0
-   }
+    }   
+
 }
